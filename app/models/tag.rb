9424f124de977b0f0f7162698685e6ac7e04a50d
chewy <chewy@MilleniumFalcon.(none)>
2012-12-28 17:01:10 -0500

    added tags listing

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,7 +1,7 @@
 class Tag < ActiveRecord::Base
   #establish relationships between tables
   attr_accessible :name
-  has_many  :taggings
+  has_many  :taggings, dependent: :destroy
   has_many  :articles, through: :taggings
 
   	def to_s
