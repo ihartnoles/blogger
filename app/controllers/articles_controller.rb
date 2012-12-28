36a0742135007ded3d28e67b8f702db31ab08c4f
chewy <chewy@MilleniumFalcon.(none)>
2012-12-28 08:35:31 -0500

    first commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,21 @@
+class ArticlesController < ApplicationController
+  def index
+  	@articles = Article.all
+  end
+
+  def show
+  	@article = Article.find(params[:id])
+  end
+
+  def new
+  	@message = "Sup Peaches"
+  	@article = Article.new
+  end
+
+  def create
+  	#raise params.inspect
+  	@article = Article.new(params[:article])
+  	@article.save
+  	redirect_to article_path(@article)
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogger::Application.config.secret_token = '30f007cb099269f196df49a50e88f1971bf3dcfd696e84ead3a77e447f81ac63d72aed7635323332cba35d7f12f8d4c373669e7da030b6b5561e60bc43d1aa88'
