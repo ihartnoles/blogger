31da4e4e3c60c4e5ada5024fe1884f3c740a9ab9
ihartnoles <ihartnoles@gmail.com>
2013-01-05 09:13:58 -0500

    Adding logging in features

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Blogger::Application.routes.draw do
+  resources :authors
+
+
   #get "tags/index"
   #get "tags/show"
-
+  root to: 'articles#index'
+  
   resources :articles
   resources :comments
   resources :tags
+  resources :author_sessions
+
+  match 'login' => 'author_sessions#new', as: :login
+  match 'logout' => 'author_sessions#destroy', as: :logout
 end
